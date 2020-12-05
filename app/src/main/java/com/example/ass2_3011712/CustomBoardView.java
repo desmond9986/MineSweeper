3477525c38c5496b7201815d71a0bea66bf97902
Desmond <desmond9986@gmail.com>
2020-12-05 21:24:45 +0800

    Milestone 1: define the shell of a custom view and layout for main activity

---
+++ app/src/main/java/com/example/ass2_3011712/CustomBoardView.java
@@ -0,0 +1,23 @@
+package com.example.ass2_3011712;
+
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CustomBoardView extends View {
+    public CustomBoardView(Context context) {
+        super(context);
+    }
+
+    public CustomBoardView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomBoardView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+}
